e3e1b2cff1d38c2589b82661f528da0eed5784ed
crojack <49529443+crojack@users.noreply.github.com>
2025-12-20 04:47:10 +0100

    Delete src directory

--- src/sql/create-playback_position-table.sql
+++
@@ -1,9 +0,0 @@
--- SPDX-FileCopyrightText: 2025 George Florea Bănuș <georgefb899@gmail.com>
--- SPDX-License-Identifier: CC-BY-4.0
-
-CREATE TABLE playback_position (
-    md5_hash TEXT NOT NULL UNIQUE,
-    path     TEXT NOT NULL,
-    position REAL NOT NULL,
-    PRIMARY KEY(md5_hash)
-);
